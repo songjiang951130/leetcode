889a27ac72d03ac1dae336236a8a79e473ec11d9
songjiang <songjiang@100tal.com>
2019-05-28 10:04:28 +0800

    去除外层括号

---
+++ src/main/java/stack/Leetcode1021.java
@@ -0,0 +1,37 @@
+package stack;
+
+import java.util.Stack;
+
+public class Leetcode1021 {
+
+    /**
+     * 移除最外层的 ()
+     * 思路 计算 左括号 和 右括号数 相等时，即可去除最外层的括号
+     *
+     * @param S
+     * @return
+     */
+    public String removeOuterParentheses(String S) {
+        StringBuilder sb = new StringBuilder();
+        int left = 0;
+        int right = 0;
+        char[] chars = S.toCharArray();
+        for (int i = 0; i < chars.length; i++) {
+            char c = chars[i];
+            if (c == '(') {
+                left++;
+            }
+            if (c == ')') {
+                right++;
+            }
+
+            if (left == right) {
+                sb.append(S.substring(i - left * 2 + 2, i));
+                left = 0;
+                right = 0;
+            }
+        }
+
+        return sb.toString();
+    }
+}
---
+++ src/test/java/Leetcode1021Test.java
@@ -0,0 +1,27 @@
+import org.junit.Test;
+import stack.Leetcode1021;
+
+import static org.junit.Assert.assertEquals;
+
+public class Leetcode1021Test {
+    @Test
+    public void testCase1() {
+        Leetcode1021 code = new Leetcode1021();
+        String s = "()()";
+        assertEquals("", code.removeOuterParentheses(s));
+    }
+
+    @Test
+    public void testCase2() {
+        Leetcode1021 code = new Leetcode1021();
+        String s = "(()())(())";
+        assertEquals("()()()", code.removeOuterParentheses(s));
+    }
+
+    @Test
+    public void testCase3() {
+        Leetcode1021 code = new Leetcode1021();
+        String  s = "(()())(())(()(()))";
+        assertEquals("()()()()(())", code.removeOuterParentheses(s));
+    }
+}
