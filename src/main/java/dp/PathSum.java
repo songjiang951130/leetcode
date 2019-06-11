67735b46e07b654cc80965d170bb6c2aab018c7b
songjiang <songjiang@100tal.com>
2019-06-11 14:49:32 +0800

    easy understand

--- src/main/java/dp/PathSum.java
+++ src/main/java/dp/PathSum.java
@@ -4,10 +4,12 @@ public class PathSum {
 
     /**
      * 当前时间 19-06-10 20:24
-     * Runtime: 4 ms, faster than 53.31% of Java online submissions for Minimum Falling Path Sum.
-     * Memory Usage: 39.3 MB, less than 68.92% of Java online submissions for Minimum Falling Path Sum.
-     *
+     * <p>
+     * Runtime: 3 ms, faster than 91.95% of Java online submissions for Minimum Falling Path Sum.
+     * Memory Usage: 37.5 MB, less than 96.67% of Java online submissions for Minimum Falling Path Sum.
+     * </p>
      * leetcode 上有直接在原数组的基础上进行dp的
+     *
      * @param A
      * @return
      */
@@ -19,32 +21,24 @@ public class PathSum {
                 //init dp
                 if (i == 0) {
                     dp[0][j] = A[0][j];
-                    if (i == A.length - 1) {
-                        min = Math.min(dp[i][j], min);
-                    }
                     continue;
                 }
                 //起始列判断
                 if (j == 0) {
                     dp[i][j] = Math.min(dp[i - 1][j], dp[i - 1][j + 1]) + A[i][j];
-                    if (i == A.length - 1) {
-                        min = Math.min(dp[i][j], min);
-                    }
                     continue;
                 }
                 //最后列判断
                 if (j == A[i].length - 1) {
                     dp[i][j] = Math.min(dp[i - 1][j], dp[i - 1][j - 1]) + A[i][j];
-                    if (i == A.length - 1) {
-                        min = Math.min(dp[i][j], min);
-                    }
                     continue;
                 }
                 dp[i][j] = Math.min(Math.min(dp[i - 1][j], dp[i - 1][j - 1]), dp[i - 1][j + 1]) + A[i][j];
-                if (i == A.length - 1) {
-                    min = Math.min(dp[i][j], min);
-                }
+
             }
+        }
+        for (int lastRow = 0; lastRow < A[A.length - 1].length; lastRow++) {
+            min = Math.min(dp[A.length - 1][lastRow], min);
         }
         return min;
     }
