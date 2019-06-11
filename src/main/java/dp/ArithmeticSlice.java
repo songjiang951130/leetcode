344de843380c49641df771fa81a7e2a851fc9fad
songjiang <songjiang@100tal.com>
2019-06-11 21:02:34 +0800

    wait

---
+++ src/main/java/dp/ArithmeticSlice.java
@@ -0,0 +1,14 @@
+package dp;
+
+public class ArithmeticSlice {
+    //求 等差数列（3个元素及以上）的 数量
+
+    /**
+     * @link https://leetcode.com/problems/arithmetic-slices/
+     * @param A
+     * @return
+     */
+    public int numberOfArithmeticSlices(int[] A) {
+        return 0;
+    }
+}
---
+++ src/test/java/dp/ArithmeticSliceTest.java
@@ -0,0 +1,15 @@
+package dp;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ArithmeticSliceTest {
+
+    @Test
+    public void numberOfArithmeticSlices() {
+        ArithmeticSlice obj = new ArithmeticSlice();
+        int[] a = {1, 2, 3, 4};
+
+    }
+}
