b65d7f8844495e92878a1b1f8eaf2339e6b32e5a
Dream <songjiang951130@gmail.com>
2019-07-06 23:23:46 +0800

    ac uniquePathsWithObstacles

--- src/main/java/dp/UniqPath.java
+++ src/main/java/dp/UniqPath.java
@@ -4,10 +4,11 @@ public class UniqPath {
 
     /**
      * 动态规划
-     * @link https://leetcode.com/problems/unique-paths
+     *
      * @param m
      * @param n
      * @return
+     * @link https://leetcode.com/problems/unique-paths
      */
     public int uniquePaths(int m, int n) {
         int[][] array = new int[m][n];
@@ -21,5 +22,40 @@ public class UniqPath {
             }
         }
         return array[m - 1][n - 1];
+    }
+
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        int[][] array = new int[obstacleGrid.length][obstacleGrid[0].length];
+        for (int i = 0; i < obstacleGrid.length; i++) {
+            for (int j = 0; j < obstacleGrid[i].length; j++) {
+                if (obstacleGrid[i][j] != 1) {
+                    if (i == 0 && j == 0) {
+                        array[i][j] = 1;
+                        continue;
+                    }
+                    // 设置第一行
+                    if (i == 0) {
+                        array[0][j] = obstacleGrid[0][j - 1] == 1 ? 0 : array[0][j - 1];
+                        continue;
+                    }
+                    // 设置第一列
+                    if (j == 0) {
+                        array[i][0] = obstacleGrid[i - 1][0] == 1 ? 0 : array[i - 1][0];
+                        continue;
+                    }
+                    int left = array[i][j - 1];
+                    if (obstacleGrid[i][j - 1] == 1) {
+                        left = 0;
+                    }
+                    int right = array[i - 1][j];
+                    if (obstacleGrid[i - 1][j] == 1) {
+                        right = 0;
+                    }
+                    array[i][j] = left + right;
+                }
+
+            }
+        }
+        return array[obstacleGrid.length - 1][obstacleGrid[0].length - 1];
     }
 }
