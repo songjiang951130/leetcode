43808d24ba5cde4bc6426c92c7356b69f70e1a0f
Dream <songjiang951130@gmail.com>
2019-07-06 11:58:32 +0800

    'ac

--- src/main/java/dp/UniqPath.java
+++ src/main/java/dp/UniqPath.java
@@ -2,10 +2,17 @@ package dp;
 
 public class UniqPath {
 
+    /**
+     * 动态规划
+     * @link https://leetcode.com/problems/unique-paths
+     * @param m
+     * @param n
+     * @return
+     */
     public int uniquePaths(int m, int n) {
         int[][] array = new int[m][n];
         for (int i = 0; i < array.length; i++) {
-            for (int j = 0; i < array[i].length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
                 if (i == 0 || j == 0) {
                     array[i][j] = 1;
                 } else {
