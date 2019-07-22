f02b824a7ebbf5f9d003ba96b17eebf9a664ba2b
songjiang <songjiang@100tal.com>
2019-07-22 11:23:32 +0800

    二叉树最大深度

---
+++ src/main/java/dfs/TreeProblem.java
@@ -0,0 +1,14 @@
+package dfs;
+
+import database.TreeNode;
+
+public class TreeProblem {
+    public int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int left = maxDepth(root.left) + 1;
+        int right = maxDepth(root.right) + 1;
+        return Math.max(left, right);
+    }
+}
