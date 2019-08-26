6ed58ee78eb1444ad291145a3a71448fd37e61a7
songjiang <songjiang@100tal.com>
2019-08-26 17:31:47 +0800

    test case

--- src/test/java/com/github/songjiang951130/leetcode/backtrack/CombinationSumTest.java
+++ src/test/java/com/github/songjiang951130/leetcode/backtrack/CombinationSumTest.java
@@ -1,6 +1,11 @@
 package com.github.songjiang951130.leetcode.backtrack;
 
+import org.junit.Assert;
 import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import static org.junit.Assert.*;
 
@@ -9,6 +14,15 @@ public class CombinationSumTest {
     @Test
     public void combinationSum() {
         CombinationSum combinationSum = new CombinationSum();
-        combinationSum.combinationSum(new int[]{2, 3, 5, 7}, 7);
+
+        Integer[] value1 = {2, 2, 3};
+        List<Integer> list1 = Arrays.asList(value1);
+        Integer[] value2 = {7};
+        List<Integer> list2 = Arrays.asList(value2);
+        List<List<Integer>> case1 = new ArrayList<>();
+        case1.add(list1);
+        case1.add(list2);
+
+        Assert.assertEquals(case1, combinationSum.combinationSum(new int[]{2, 3, 6, 7}, 7));
     }
 }
