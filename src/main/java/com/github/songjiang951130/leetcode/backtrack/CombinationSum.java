c1773082823d0ef5e31d8099ce366742df0341fb
songjiang <songjiang@100tal.com>
2019-08-26 17:24:12 +0800

    凑数

---
+++ src/main/java/com/github/songjiang951130/leetcode/backtrack/CombinationSum.java
@@ -0,0 +1,43 @@
+package com.github.songjiang951130.leetcode.backtrack;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CombinationSum {
+    /**
+     * case 1:
+     * Input: candidates = [2,3,6,7], target = 7,
+     * A solution set is:
+     * [
+     * [7],
+     * [2,2,3]
+     * ]
+     *
+     * @param candidates
+     * @param target
+     * @return
+     */
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        List<List<Integer>> result = new ArrayList<>();
+        backTrace(candidates, target, result, new ArrayList<>(), 0);
+        return result;
+    }
+
+    private void backTrace(int[] candidates, int target, List<List<Integer>> result, List<Integer> save, int index) {
+        if (target < 0) {
+            return;
+        }
+        if (0 == target) {
+            //save 是引用类型，其他调用栈会影响该值
+            result.add(new ArrayList<>(save));
+            return;
+        }
+
+        for (int i = index; i < candidates.length; i++) {
+            save.add(candidates[i]);
+            backTrace(candidates, target - candidates[i], result, save, i);
+            //新加入的元素移除 继续回溯
+            save.remove(save.size() - 1);
+        }
+    }
+}
---
+++ src/test/java/com/github/songjiang951130/leetcode/backtrack/CombinationSumTest.java
@@ -0,0 +1,14 @@
+package com.github.songjiang951130.leetcode.backtrack;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CombinationSumTest {
+
+    @Test
+    public void combinationSum() {
+        CombinationSum combinationSum = new CombinationSum();
+        combinationSum.combinationSum(new int[]{2, 3, 5, 7}, 7);
+    }
+}
