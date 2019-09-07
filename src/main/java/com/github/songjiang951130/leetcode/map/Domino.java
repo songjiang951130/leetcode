a2f606d66962ad89708773b9af87334ed1b772e0
Dream <songjiang951130@gmail.com>
2019-09-07 10:30:01 +0800

    ok

---
+++ src/main/java/com/github/songjiang951130/leetcode/map/Domino.java
@@ -0,0 +1,8 @@
+package com.github.songjiang951130.leetcode.map;
+
+public class Domino{
+    public int numEquivDominoPairs(int[][] dominoes) {
+        Map<Integer,Integer> map = new HashMap<>();
+        return 0;
+    }
+}
