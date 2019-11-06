5401e17ad76bc607ffdeaa732db0ef872c78fafc
songjiang <songjiang@100tal.com>
2019-11-06 11:23:19 +0800

    栈解决有效括号问题

--- src/test/java/com/github/songjiang951130/leetcode/stack/ParenthesesTest.java
+++ src/test/java/com/github/songjiang951130/leetcode/stack/ParenthesesTest.java
@@ -8,10 +8,10 @@ import java.util.List;
 import static org.junit.Assert.*;
 
 public class ParenthesesTest {
+    Parentheses parentheses = new Parentheses();
 
     @Test
     public void generateParenthesis() {
-        Parentheses parentheses = new Parentheses();
         int case1 = 3;
         List<String> list1 = new ArrayList<String>();
         list1.add("((()))");
@@ -20,5 +20,15 @@ public class ParenthesesTest {
         list1.add("()(())");
         list1.add("()()()");
         assertEquals(list1, parentheses.generateParenthesis(case1));
+    }
+
+    @Test
+    public void isValid() {
+        assertTrue(parentheses.isValid("((()))"));
+        assertTrue(parentheses.isValid("({}()[])"));
+        assertFalse(parentheses.isValid("({})]"));
+        assertFalse(parentheses.isValid("({})["));
+        assertFalse(parentheses.isValid("["));
+
     }
 }
