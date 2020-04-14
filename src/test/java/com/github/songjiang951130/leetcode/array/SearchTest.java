52738bb32ed189203fdec8d7e39bfee5406fe8ea
songjiang951130 <songjiang951130@gmail.com>
2020-04-14 15:23:24 +0800

    旋转数组搜索问题

---
+++ src/test/java/com/github/songjiang951130/leetcode/array/SearchTest.java
@@ -0,0 +1,20 @@
+package com.github.songjiang951130.leetcode.array;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SearchTest {
+    Search search = new Search();
+
+    @Test
+    public void search() {
+        Assert.assertEquals(5, search.search(new int[]{6, 7, 1, 2, 3, 4, 5}, 4));
+        /**
+         * [4,5,6,7,0,1,2]
+         * 0
+         */
+        Assert.assertEquals(0, search.search(new int[]{4, 5, 6, 7, 0, 1, 2}, 4));
+    }
+}
