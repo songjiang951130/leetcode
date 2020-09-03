1131468ce06d90b6ceb6655f770bcfa1791ae124
songjiang <songjiang@100tal.com>
2020-09-03 15:23:22 +0800

    tree

---
+++ src/main/java/com/github/songjiang951130/leetcode/tree/SameTree.java
@@ -0,0 +1,18 @@
+package com.github.songjiang951130.leetcode;
+
+import com.github.songjiang951130.leetcode.database.TreeNode;
+
+public class SameTree {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null) {
+            return true;
+        }
+        if (p == null || q == null) {
+            return false;
+        }
+        if (p.val != q.val) {
+            return false;
+        }
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    }
+}
