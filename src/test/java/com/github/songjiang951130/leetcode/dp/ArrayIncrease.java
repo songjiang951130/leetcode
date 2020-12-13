845ffc18714032fb353078eed5cd4a5b210259bc
Dream <songjiang951130@gmail.com>
2020-12-13 23:51:25 +0800

    最长上升子序列

---
+++ src/test/java/com/github/songjiang951130/leetcode/dp/ArrayIncrease.java
@@ -0,0 +1,24 @@
+package com.github.songjiang951130.leetcode.dp;
+
+import java.util.Arrays;
+
+public class ArrayIncrease {
+
+    public int lengthOfLIS(int[] nums) {
+        int[] dp = new int[nums.length];
+        Arrays.fill(dp, 1);
+        int res = 1;
+        for (int i = 1; i < nums.length; i++) {
+            //
+            for (int j = 0; j < i; j++) {
+                if (nums[j] < nums[i]) {
+                    dp[i] = Math.max(dp[j] + 1, dp[i]);
+                }
+            }
+            res = Math.max(res, dp[i]);
+
+        }
+        return res;
+    }
+
+}
