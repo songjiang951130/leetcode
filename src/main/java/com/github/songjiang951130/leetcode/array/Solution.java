5d5ec4446ee4d081e9df7400e8cf32d1a736f922
Dream <songjiang951130@gmail.com>
2023-12-06 09:21:20 +0800

    三数之和超时版

--- src/main/java/com/github/songjiang951130/leetcode/array/Solution.java
+++ src/main/java/com/github/songjiang951130/leetcode/array/Solution.java
@@ -1,5 +1,10 @@
 package com.github.songjiang951130.leetcode.array;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 class Solution {
     public int removeDuplicates(int[] nums) {
         if (nums == null || nums.length == 0) {
@@ -17,6 +22,43 @@ class Solution {
             }
         }
         return len;
+    }
 
+    /**
+     * 三数之和，超时了
+     *
+     * @param nums 数组
+     * @return 三数之和
+     */
+    public List<List<Integer>> threeSum(int[] nums) {
+        Arrays.sort(nums);
+        List<List<Integer>> res = new ArrayList<>();
+        for (int i = 0; i < nums.length; i++) {
+            //优化当前值大于0，说明前后之合数已经大于0
+            if (nums[i] > 0) {
+                break;
+            }
+            int s = i + 1;
+            int e = nums.length - 1;
+            while (s < e) {
+                int sum = nums[i] + nums[s] + nums[e];
+                if (sum == 0) {
+                    List<Integer> el = new ArrayList<>();
+                    el.add(nums[i]);
+                    el.add(nums[s]);
+                    el.add(nums[e]);
+                    if (!res.contains(el)) {
+                        res.add(el);
+                    }
+                    s++;
+                    e--;
+                } else if (sum < 0) {
+                    s++;
+                } else {
+                    e--;
+                }
+            }
+        }
+        return res;
     }
 }
--- src/test/java/com/github/songjiang951130/leetcode/array/SolutionTest.java
+++ src/test/java/com/github/songjiang951130/leetcode/array/SolutionTest.java
@@ -1,7 +1,11 @@
 package com.github.songjiang951130.leetcode.array;
 
+import com.google.common.collect.Lists;
 import org.junit.Assert;
 import org.junit.Test;
+
+import java.util.Collections;
+import java.util.List;
 
 public class SolutionTest {
     Solution solution = new Solution();
@@ -11,5 +15,25 @@ public class SolutionTest {
         Assert.assertEquals(4, solution.removeDuplicates(new int[]{1, 2, 3, 4}));
         Assert.assertEquals(2, solution.removeDuplicates(new int[]{1, 2, 2}));
         Assert.assertEquals(2, solution.removeDuplicates(new int[]{1, 1, 2}));
+    }
+
+    @Test
+    public void testThreeSum() {
+        List<List<Integer>> res1 = Lists.newArrayList(
+                Lists.newArrayList(-1, -1, 2),
+                Lists.newArrayList(-1, 0, 1)
+
+        );
+        Assert.assertEquals(res1,
+                solution.threeSum(new int[]{0, 1, -1, -1, 2, -1, -4}));
+
+        List<List<Integer>> res2 = Lists.newArrayList(
+                Lists.newArrayList(-2, 0, 2),
+                Lists.newArrayList(-2, 1, 1)
+        );
+        Assert.assertEquals(res2,
+                solution.threeSum(new int[]{-2, 0, 1, 1, 2}));
+
+
     }
 }
