d8592c078f521b2a43add7e118d282c22ef2ec10
Dream <songjiang951130@gmail.com>
2025-02-09 18:40:05 +0800

    牛客自测

--- src/main/java/com/github/songjiang951130/TreeBuild.java
+++ src/main/java/com/github/songjiang951130/TreeBuild.java
@@ -9,30 +9,29 @@ import java.util.LinkedList;
  * @see
  */
 public class TreeBuild {
-    public TreeNode build(int[] preorder, int[] inorder) {
-        if (preorder == null || preorder.length == 0) {
-            return null;
+    public static TreeNode constructTreeFromArray(int[] nums) {
+        if (nums.length == 0) return null; // 如果数组为空，则返回null
+
+        TreeNode root = null; // 初始化根节点为null
+
+        for (int num : nums) {
+            root = insertIntoBST(root, num); // 插入每个数到BST中
         }
-        TreeNode root = new TreeNode(preorder[0]);
-        Deque<TreeNode> stack = new LinkedList<TreeNode>();
-        stack.push(root);
-        int inorderIndex = 0;
-        for (int i = 1; i < preorder.length; i++) {
-            int preorderVal = preorder[i];
-            TreeNode node = stack.peek();
-            if (node.val != inorder[inorderIndex]) {
-                node.left = new TreeNode(preorderVal);
-                stack.push(node.left);
-            } else {
-                while (!stack.isEmpty() && stack.peek().val == inorder[inorderIndex]) {
-                    node = stack.pop();
-                    inorderIndex++;
-                }
-                node.right = new TreeNode(preorderVal);
-                stack.push(node.right);
-            }
+
+        return root; // 返回构造好的树的根节点
+    }
+
+    public static TreeNode insertIntoBST(TreeNode root, int val) {
+        // 如果树是空的，则返回新的节点作为根节点
+        if (root == null) {
+            return new TreeNode(val);
+        }
+        // 根据当前节点值与插入值比较，决定向左或向右子树递归插入
+        if (val < root.val) {
+            root.left = insertIntoBST(root.left, val);
+        } else {
+            root.right = insertIntoBST(root.right, val);
         }
         return root;
     }
-
 }
